f00d6b8419125d0aa955a82e7cd393ad8708ae25
Hiott112358 <114514463+Hiott112358@users.noreply.github.com>
2022-11-13 17:45:13 +0300

    Delete Homework-1 directory

--- Homework-1/Task-2.py
+++
@@ -1,8 +0,0 @@
-def warn_the_sheep(queue):
-    if 'wolf' == queue[-1]:
-        return "Pls go away and stop eating my sheep"
-    else:
-        n = str(len(queue) - queue.index('wolf') - 1)
-        Ph = list("Oi! Sheep number N! You are about to be eaten by a wolf!")
-        Ph[17], n = n, Ph[17]
-        return(''.join(Ph))
